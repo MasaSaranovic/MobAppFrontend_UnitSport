ae9fd4f8be0d5f8a308d580c41658b3aa8346249
Masa Saranovic <masasaranovic@gmail.com>
2022-01-11 07:43:43 +0100

    Front

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { LogBox } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+
+// Navigators
+import Main from './Navigators/Main'
+
+// Screens
+import ProductContainer from './Screens/Products/ProductContainer'
+import Header from './Shared/Header'
+
+LogBox.ignoreAllLogs(true);
+
+export default function App() {
+  return (
+    <NavigationContainer>
+        <Header />
+        <Main />
+    </NavigationContainer>
+
+  );
+}
+
